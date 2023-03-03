52ec7387ab4a823eb9ede7221d5232dad7370b4a
Juan Carlos <juancapdipp@gmail.com>
2023-03-03 09:25:59 -0300

    feat: error handler for places

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -21,7 +21,12 @@ router.get('/:pid', (req, res, next) => {
 	const placeId = req.params.pid;
 	const place = DUMMY_PLACES.find((place) => {
 		return place.id === placeId;
-	});  
+	});
+	if(!place) {
+		return res
+			.status(404)
+			.json({ message: 'Could not find a place for the provided id.' });
+	}
 	res.json({ place });
 });
 
@@ -30,6 +35,11 @@ router.get('/users/:uid', (req, res, next) => {
 	const place = DUMMY_PLACES.find((place) => {
 		return place.creatorId === userId;
 	});
+	if(!place) {
+		return res
+			.status(404)
+			.json({ message: 'Could not find a place for the provided user id.' });
+	}
 	res.json({ place });
 });
 
