32b469fcd40a52d6e70c53d9de25c82ca17993b3
Juan Carlos <juancapdipp@gmail.com>
2023-03-03 09:46:55 -0300

    feat: add model for http error

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const HttpError = require('../models/http-error');
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -23,9 +23,7 @@ router.get('/:pid', (req, res, next) => {
 		return place.id === placeId;
 	});
 	if(!place) {
-		const error = new Error('Could not find a place for the provided id.');
-		error.code = 404;
-		throw error;
+		throw new HttpError('Could not find a place for the provided id.', 404);
 	}
 	res.json({ place });
 });
@@ -36,9 +34,9 @@ router.get('/users/:uid', (req, res, next) => {
 		return place.creatorId === userId;
 	});
 	if(!place) {
-		const error = new Error('Could not find a place for the provided user id.');
-		error.code = 404;
-		return next(error);
+		return next(
+			new HttpError('Could not find a place for the provided user id.', 404)
+		);
 	}
 	res.json({ place });
 });
